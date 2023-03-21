cb72bb4b7d38721a5df44ddf721734dfad0d026f
Rohan C. Shah <rcs353@cornell.edu>
2023-03-20 21:29:39 -0400

    load tipico page

--- scraping.py
+++ scraping.py
@@ -3,4 +3,17 @@ from datetime import datetime
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from bs4 import BeautifulSoup
+import urllib
+import re
+
+url = "https://www.tipico.com/us"
+try:
+    page = urllib.request.urlopen(url)
+except:
+    print("error")
+
+soup = BeautifulSoup(page, 'html.parser')
+
+# storing relevant information, waiting on Tipico account
 
